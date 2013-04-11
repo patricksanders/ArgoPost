6fc2e1318077825dd6dc7d61bb4fc0df1301d8ce
Anthony <anthony@centrawebs.com>
2013-04-10 21:46:04 -0500

    subscrip

--- subscrip.js
+++ subscrip.js
@@ -3,14 +3,25 @@ function createSubscription() {
 	var aThreadID = document.getElementById("threads");
 	//get forum title from dropdown menu in the post UI
 	var aForumTitle = document.getElementById("forums");
+	//this variable is used to check if a field was left empty
+	var isEmptyField = false;
 	
+	//check that a thread has been selected
+	if(aThreadID.value == "Select a thread"){
+		isEmptyField = true;
+	}
+	//check that a forum has been selected
+	if(aForumTitle.value == "Select a forum"){
+		isEmptyField = true;
+	}
+	else {
 		$.ajax({
 			type : "GET",
 			url : "Subscriber.cfc?wsdl&method=AddToSubscriptions&ThreadID="+aThreadID.value,
 			success : subscriptionSucceeded,
 			failure : failedToAddSubscription
 		});
-
+	}
 }
 
 function subscriptionSucceeded(response) {
