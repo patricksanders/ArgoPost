3d016f871ee7d9d84bfa04677a1d1b6a15c75c9b
Andy Milbeck <ajm46@students.uwf.edu>
2013-04-16 11:27:43 -0500

    still testing code
    previous version was not passing variables correctly

--- createPost.js
+++ createPost.js
@@ -36,11 +36,12 @@
 			url : "Post.cfc?wsdl&method=addPost",
 			contentType : "application/json; charset=utf-8",
 			processData: false,
-			data: "postTitle="+aTitle.value+"&postContent="+aDescription.value+"&threadID="+aThreadID.value,
-			dataType : "json",
-			success : postSucceeded,
-			failure : failedToAddPost
-		});
+			data: {postTitle: aTitle.value, postContent: aDescription.value, threadID: aThreadID.value},
+			dataType : "json"
+			//success : postSucceeded,
+			//failure : failedToAddPost
+		}).done(function(result){
+			console.log(result)};
 	}
 }
 
