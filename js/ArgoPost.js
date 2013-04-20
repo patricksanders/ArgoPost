1976263d5167829eb3209df8ff00ec75b9b51d02
Ryan Whitwell <Ryan@OSX-001.local>
2013-04-20 10:56:45 -0500

    updated delete functionality (cont.)
    Changed javascript method calls to deleteArgoPost instead of markExpired

--- js/ArgoPost.js
+++ js/ArgoPost.js
@@ -40,7 +40,7 @@ function getArgoPostSearchResults()
 function deleteArgoPostFromThread(postId, threadId)
 {
 	$.ajax({
-		type: "GET", url: "argopostdelete.cfc?wsdl&method=markExpired"
+		type: "GET", url: "argopostdelete.cfc?wsdl&method=deleteArgoPost"
 				+"&postId=" + postId + "&threadId=" + threadId,
 		contentType: "application/json; charset=utf-8",
 		dataType: "json",
@@ -53,7 +53,7 @@ function deleteArgoPostFromThread(postId, threadId)
 function deleteArgoPostFromSearch(postId)
 {
 	$.ajax({
-		type: "GET", url: "argopostdelete.cfc?wsdl&method=markExpired"
+		type: "GET", url: "argopostdelete.cfc?wsdl&method=deleteArgoPost"
 				+"&postID=" + postId + "&threadId=-1",
 		contentType: "application/json; charset=utf-8",
 		dataType: "json",
